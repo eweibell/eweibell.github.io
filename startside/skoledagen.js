50ad95de230a0828c299de3cb567ca283035ba81
eweibell <54504318+eweibell@users.noreply.github.com>
2023-09-20 09:49:41 +0200

    Beta 1.4
    Automatic recognittion of what week it is

--- startside/skoledagen.js
+++ startside/skoledagen.js
@@ -6,6 +6,30 @@ const minFromTo = (start, end) => {
     return min
 }
 
+
+
+
+function isEven(num) {
+  return (num % 2 == 0);
+}
+
+const currentDate = new Date();
+const startDate = new Date(currentDate.getFullYear(), 0, 1);
+const days = Math.floor((currentDate - startDate) /
+    (24 * 60 * 60 * 1000));
+    
+var weekNumber = Math.ceil(days / 7);
+
+if (isEven(weekNumber)) {
+    ukeNum = timeFagU2
+}
+else {
+    ukeNum = timeFagU1
+}
+
+
+
+
 const ukedager = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag']
 
 const timeTider = {
@@ -79,9 +103,9 @@ const renderSkoletimer = (containerDiv, renderTimestamp) => {
             skoletimeDiv.className = 'skoletime ferdig'
         }
         const contentKlokke = document.createTextNode(`${tt[0]}:${tt[1]}-${tt[2]}:${tt[3]}`)
-        const contentFag = document.createTextNode(`${timeFagU1[ukedager[ukedag]][index]}`)
+        const contentFag = document.createTextNode(`${ukeNum[ukedager[ukedag]][index]}`)
         fagDiv.appendChild(contentFag)
-        if (timeFagU2[ukedager[ukedag]][index] !== '') {
+        if (ukeNum[ukedager[ukedag]][index] !== '') {
             klokkeDiv.appendChild(contentKlokke)
         }
         skoletimeDiv.appendChild(fagDiv)
